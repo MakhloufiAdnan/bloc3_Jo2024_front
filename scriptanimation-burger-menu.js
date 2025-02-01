a1ecb95dae60ab792e2b68792869281654630adb
MakhloufiAdnan <makhloufiadnan@gmail.com>
2025-02-01 19:16:47 +0100

    medif-hedear mobil first+add header mobile

---
+++ scriptanimation-burger-menu.js
@@ -0,0 +1,8 @@
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
+
+// Toggle "open" class on burger and menu
+burger.addEventListener("click", () => {
+  burger.classList.toggle("open");
+  menu.classList.toggle("open");
+});
