b535ad79f0673b8ebcee6cbfc31c1750cc6339ce
MakhloufiAdnan <makhloufiadnan@gmail.com>
2025-06-12 17:57:59 +0200

    sup /

--- dist/js/api/config.js
+++ dist/js/api/config.js
@@ -2,7 +2,7 @@ let determinedApiUrl;
 
 // Vérifiez si le frontend est servi depuis votre domaine Heroku de production
 if (window.location.hostname === 'bloc3-jo2024-front-eff05b08aaa7.herokuapp.com') {
-    determinedApiUrl = 'https://bloc3-jo2024-back-1b523dbede86.herokuapp.com/';
+    determinedApiUrl = 'https://bloc3-jo2024-back-1b523dbede86.herokuapp.com';
 } else {
     // Sinon, supposez que c'est un environnement de développement local
     determinedApiUrl = 'http://localhost:8080';
